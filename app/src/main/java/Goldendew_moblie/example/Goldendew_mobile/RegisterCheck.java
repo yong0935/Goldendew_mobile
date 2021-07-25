0c76f771bd385145bfcbd2f7e459a9f40847ef50
Yonghoo Lee <76414897+yong0935@users.noreply.github.com>
2021-07-26 08:59:48 +0900

    Update RegisterCheck.java

--- app/src/main/java/Goldendew_moblie/example/Goldendew_mobile/RegisterCheck.java
+++ app/src/main/java/Goldendew_moblie/example/Goldendew_mobile/RegisterCheck.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class RegisterCheck extends StringRequest {
 
     //서버 url 설정(php 파일 연동)
-    final static private String URL = "http://192.1.1.232/Login1.php";
+    final static private String URL = "Login";
     private Map<String, String> map;
 
 
